99ca5077be1b008b780413285ee90f22629eb3c6
satchit8 <satchit8@tutanota.com>
2016-05-31 06:46:54 +0000

    Use *a and format code conveniently
    Tips
    
    0. Ensure various Clojure on Android development pieces are set-up
       and briefly scan the References section below.
    
    1. Build and run the app to see what it does.
    
    2. In main.clj, note the following:
    
         (let [
               this (*a)
              ]
              ...
               [:text-view {:text "Hi there!"
               ...
         ) ;
    
       With a CIDER connection to the running app, try changing the string
       value associated with :text and execute C-x C-e (Eval last sexp) on
       the line following ") ;"
    
    References
    
    http://www.lambdacat.com/clojure-on-android-overview/
    http://www.lambdacat.com/clojure-android-workflow-setup/
    http://www.lambdacat.com/clojure-on-android-workflow-setup-ii/
    https://github.com/clojure-android/lein-droid/wiki/Tutorial
    https://github.com/clojure-android/neko/wiki
    https://github.com/clojure-android/neko/wiki/Namespaces#user-content-defining-an-activity

--- src/clojure/org/example/basic/main.clj
+++ src/clojure/org/example/basic/main.clj
@@ -1,20 +1,25 @@
 (ns org.example.basic.main
   (:require [neko.activity :refer [defactivity
                                    set-content-view!]]
+            [neko.debug :refer [*a]]
             [neko.threading :refer [on-ui]]
-  )
-  (:import android.widget.TextView
   )
 )
 
 (defactivity org.example.basic.MainActivity
+  :key :main
 
   (onCreate [this bundle]
     (.superOnCreate this bundle)
-    (on-ui
-      (set-content-view! this
-        [:text-view {:text "Hi there!"
-                     :text-size (float 64)}]))
+    (let [
+          this (*a)
+         ]
+      (on-ui
+        (set-content-view! this
+          [:text-view {:text "Hi there!"
+                       :text-size (float 64)}]))
+    ) ;
+
   )
 
 )

288b67b44c30cf4b6c5cc7fdd9f350987e380cc9
satchit8 <satchit8@tutanota.com>
2016-06-05 07:21:44 +0000

    Use find-view and :id trait
    Tips
    
    0. Ensure various Clojure on Android development pieces are set-up
       and briefly scan the References section below.
    
    1. Build and run the app to see what it does.
    
    2. In main.clj, note the following:
    
         let [
              ...
              label-text-a "A Nice Label"
              label-text-b "THE Nice Label"
             ]
             ...
               [:text-view {:id ::a-tv
                            :text label-text-a
               ...
               [:button {:on-click (fn [_]
               ...
                                     (when-let [a-tv (find-view this ::a-tv)]
                                       (.setText a-tv
                                         (if (= (.getText a-tv)
                                               label-text-a)
                                           label-text-b
                                           label-text-a))))
    
    References
    
    http://www.lambdacat.com/clojure-on-android-overview/
    http://www.lambdacat.com/clojure-android-workflow-setup/
    http://www.lambdacat.com/clojure-on-android-workflow-setup-ii/
    https://github.com/clojure-android/lein-droid/wiki/Tutorial
    https://github.com/clojure-android/neko/wiki
    https://github.com/clojure-android/neko/wiki/Namespaces#user-content-nekofind-view
    https://github.com/clojure-android/neko/wiki/User-interface#user-content-list-of-most-useful-traits
    http://kotka.de/blog/2010/05/Did_you_know_III.html

--- src/clojure/org/example/basic/main.clj
+++ src/clojure/org/example/basic/main.clj
@@ -2,6 +2,7 @@
   (:require [neko.activity :refer [defactivity
                                    set-content-view!]]
             [neko.debug :refer [*a]]
+            [neko.find-view :refer [find-view]]
             [neko.log :as log]
             [neko.notify :refer [toast]]
             [neko.threading :refer [on-ui]]
@@ -16,6 +17,8 @@
     (.superOnCreate this bundle)
     (let [
           this (*a)
+          label-text-a "A Nice Label"
+          label-text-b "THE Nice Label"
          ]
       (on-ui
         (set-content-view! this
@@ -23,10 +26,16 @@
                            :layout-height :fill
                            :layout-width :fill
                            :orientation :vertical}
-           [:text-view {:text "A Nice Label"
+           [:text-view {:id ::a-tv
+                        :text label-text-a
                         :text-size (float 32)}]
            [:button {:on-click (fn [_]
-                                 (toast "Smile?" :long))
+                                 (when-let [a-tv (find-view this ::a-tv)]
+                                   (.setText a-tv
+                                     (if (= (.getText a-tv)
+                                           label-text-a)
+                                       label-text-b
+                                       label-text-a))))
                      :text "Press Me"
                      :text-size (float 32)}]
           ]
